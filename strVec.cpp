daa71d58464597dfa19b19e1ff4b59f1d20fc2fb
jasonchenzhou <zhouchen@inside-65-85-146.wireless.utoronto.ca>
2015-08-28 14:33:04 -0400

    ok

--- strVec.cpp
+++ strVec.cpp
@@ -1,9 +1,16 @@
 #include "strVec.h"
 
+void strVec::push_back(const std::string &s)
+{
+    chk_n_alloc();
+    alloc.construct(first_free++, s);
+}
+
 std::pair<std::string*, std::string*> strVec::alloc_n_copy(const std::string* b, const std::string* e){
 	auto data = alloc.allocate(e -b);
 	return {data, uninitialized_copy(b, e, data)};   //return a pair, which is begin and end of the new string
 }
+
 
 void strVec::free(){
 	if(elements){
@@ -15,31 +22,80 @@ void strVec::free(){
 }
 
 strVec::strVec(const strVec &rfs){
-    auto data = alloc_n_copy(rfs);
+    auto data = alloc_n_copy(rfs.begin(), rfs.end());
     elements = data.first;
     first_free = cap = data.second;
 }
 
-strVec &operator=(const strVec &rhs){
-	auto tem = alloc_n_copy(rhs);
+strVec& strVec::operator=(const strVec &rhs){
+	auto tem = alloc_n_copy(rhs.begin(), rhs.end());
 	free();
 	elements = tem.first;
 	first_free = cap = tem.second;
 	return *this;
 }
 
-~strVec(){free();}
+strVec::~strVec(){free();}
 
-void reallocate(){
+
+void strVec::alloc_n_move(size_t new_cap)
+{
+    auto newdata = alloc.allocate(new_cap);
+    auto dest = newdata;
+    auto elem = elements;
+    for (size_t i = 0; i != size(); ++i)
+        alloc.construct(dest++, std::move(*elem++));
+    free();
+    elements = newdata;
+    first_free = dest;
+    cap = elements + new_cap;
+}
+
+void strVec::reallocate(){
 	auto newCapacity = size() ? 2*size() : 1;
 	auto newData = alloc.allocate(newCapacity);
 	auto dest = newData;
 	auto elem = elements;
 	for(size_t i=0; i!=size(); i++){
-		alloc.construct(dest++, std::move(elem++));
+		alloc.construct(dest++, std::move(*elem++));
 	}
 	free();
 	elements = newData;
 	first_free = dest;
 	cap = elements + newCapacity;
+}
+
+
+void strVec::reserve(size_t new_cap)
+{
+    if (new_cap <= capacity()) return;
+    alloc_n_move(new_cap);
+}
+
+void strVec::resize(size_t count)
+{
+    resize(count, std::string());
+}
+
+void strVec::resize(size_t count, const std::string &s)
+{
+    if (count > size()) {
+        if (count > capacity()) reserve(count * 2);
+        for (size_t i = size(); i != count; ++i)
+            alloc.construct(first_free++, s);
+    }
+    else if (count < size()) {
+        while (first_free != elements + count)
+            alloc.destroy(--first_free);
+    }
+}
+
+int main()
+{
+	strVec v;
+	v.push_back("jason");
+	v.push_back("billy");
+	std::cout<<*v.begin()<<std::endl;
+	for(auto elem : v)  std::cout<<elem<<std::endl;
+    return 0;
 }
